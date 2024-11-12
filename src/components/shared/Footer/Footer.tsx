3fec25f5463254e4adce02d821092d03e7b2ff17
Abdullah149081 <abdullah149081@hotmail.com>
2024-11-12 22:52:54 +0600

    Update project name to 'restaurant'; enhance Footer component with new background image and responsive design

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,17 +1,21 @@
+import Container from "@/components/ui/container";
+
 import Link from "next/link";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div>
-      <p className="text-center">
-        © {currentYear}{" "}
-        <Link href="/" className="text-black">
-          Restaurant
-        </Link>
-        . All rights reserved.
-      </p>
-    </div>
+    <footer className="footer-bg">
+      <Container className="h-full py-[120px]">
+        <p className="flex h-full items-center justify-center text-white">
+          © {currentYear}{" "}
+          <Link href="/" className="pl-1">
+            Restaurant
+          </Link>
+          . All rights reserved.
+        </p>
+      </Container>
+    </footer>
   );
 };
 
