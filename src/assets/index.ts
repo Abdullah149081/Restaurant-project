95e7b080dd21f81225c2387520f37ef85f473b2e
Abdullah149081 <abdullah149081@hotmail.com>
2024-11-13 22:40:10 +0600

    Replace PNG images with WEBP format for improved performance; update background images in utilities.css

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,17 +1,16 @@
-import footer from "@/assets/bg/Footer.png";
 import fb from "@/assets/icon/facebook-circle.svg";
 import instagram from "@/assets/icon/instagram-round.svg";
 import ln from "@/assets/icon/linkedin-round.svg";
 import tw from "@/assets/icon/twitter-round-new-alt.svg";
-import Banner1 from "@/assets/images/banner1.png";
-import bannerImg from "@/assets/images/banner_img.png";
-import bowlWhiteImg from "@/assets/images/bowl-white-horizontal.png";
-import burger from "@/assets/images/classic-burger.png";
-import frenchFries from "@/assets/images/french-fries.png";
-import friedShrimp from "@/assets/images/fried-shrimp.png";
-import pizza from "@/assets/images/pizza.png";
-import sandwich from "@/assets/images/sandwich.png";
-import topViewBell from "@/assets/images/top-view-bellpng.png";
+import Banner1 from "@/assets/images/banner1.webp";
+import bannerImg from "@/assets/images/banner_img.webp";
+import bowlWhiteImg from "@/assets/images/bowl-white-horizontal.webp";
+import burger from "@/assets/images/classic-burger.webp";
+import frenchFries from "@/assets/images/french-fries.webp";
+import friedShrimp from "@/assets/images/fried-shrimp.webp";
+import pizza from "@/assets/images/pizza.webp";
+import sandwich from "@/assets/images/sandwich.webp";
+import topViewBell from "@/assets/images/top-view-bellpng.webp";
 import bag from "@/assets/svgs/bag.svg";
 import callAlt from "@/assets/svgs/call-alt.svg";
 import circleWhite from "@/assets/svgs/ciccle_white.svg";
@@ -52,9 +51,6 @@ const assets = {
     bag,
     medal,
     packageIcon,
-  },
-  bg: {
-    footer,
   },
 };
 
